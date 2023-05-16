6b8ec98e008f0db0d881f0fd7498080d8db64c5c
SoftwareTalent <mobile_top@hotmail.com>
2023-05-15 21:07:23 -0500

    patch and delete decorates on controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,36 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller()
 export class AppController {
   @Get()
-  getUser() {
+  getUsers() {
     return { name: 'Thomas', email: 'thomas@example.com' };
   }
   @Post()
   store(@Req() req: Request) {
     console.log(req.body);
     return req.body;
+  }
+  @Patch()
+  update(@Req() req: Request) {
+    console.log(req.body);
+    return req.body;
+  }
+  @Get('/:userId')
+  getUser(@Param() params: { userId: number }) {
+    return params;
+  }
+  @Delete('/:userId')
+  deleteUser(@Param() params: { userId: number }) {
+    return params;
   }
 }
