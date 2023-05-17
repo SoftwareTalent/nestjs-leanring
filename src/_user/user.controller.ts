e1a5fa093a01f19faf91a126e242ddb315f18631
SoftwareTalent <mobile_top@hotmail.com>
2023-05-16 21:02:43 -0500

    create controller and module using cli

---
+++ src/_user/user.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+@Controller('/user')
+export class UserController {
+  @Get()
+  getUsers() {
+    return { name: 'Thomas', email: 'thomas@example.com' };
+  }
+  @Post()
+  store(@Req() req: Request) {
+    console.log(req.body);
+    return req.body;
+  }
+  @Patch()
+  update(@Req() req: Request) {
+    console.log(req.body);
+    return req.body;
+  }
+  @Get('/:userId')
+  getUser(@Param() params: { userId: number }) {
+    return params;
+  }
+  @Delete('/:userId')
+  deleteUser(@Param() params: { userId: number }) {
+    return params;
+  }
+}
