8f143b03bdfc186c1e67831aba64320da0699715
SoftwareTalent <mobile_top@hotmail.com>
2023-05-18 20:51:21 -0500

    passport authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
+import { Request, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private userService: UserService) {}
+  @UseGuards(AuthGuard('local'))
   @Post('/login')
-  async login(@Body() loginDto: any) {
-    const user = await this.userService.findByEmail(loginDto.email);
-    if (user) {
-      if (user.password === loginDto.password) {
-        return user;
-      }
-      return "password doesn't match";
-    }
-    return 'unauthenticated';
+  async login(@Request() req: any) {
+    return req.user;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
-  imports: [UserModule],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
